983eb243cf3a8ae6b9c63ded117bf2ea5da9d52c
Iren Peter <peterirenkat@gmail.com>
2024-06-27 18:51:55 +0200

    Controller classes Added

---
+++ Backend/Controllers/PlanDetailController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Backend.Model;
+using Backend.Model.DTO;
+using Backend.Repository;
+using Microsoft.AspNetCore.Mvc;
+using PlanDetail = Backend.Model.PlanDetail;
+
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlanDetailController : ControllerBase
+    {
+        private readonly ILogger<PlanController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public PlanDetailController(ILogger<PlanController> logger, IConfiguration configuration, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("/plandetail/{id}")]
+        public async Task<ActionResult<PlanDetail>> GetType(int id)
+        {
+            var t = await _unitOfWork.PlanDetails.GetByIdAsync(id);
+            if (t == null)
+            {
+                return NotFound("no plan with id");
+            }
+
+            return Ok(t);
+        }
+
+        [HttpGet]
+        [Route("/plandetail/all")]
+        public async Task<ActionResult<IEnumerable<PlanDetailDto>>> GetAllType()
+        {
+            var planDetails = await _unitOfWork.Types.GetAllAsync();
+            var planDetailDtos = _mapper.Map<IEnumerable<PlanDetailDto>>(planDetails);
+            return Ok(planDetailDtos);
+        }
+
+        [HttpPost]
+        [Route("/planDetail/add")]
+        public async Task<ActionResult<PlanDetail>> AddPlanDetail([FromBody] CUPlanDetailDto planDetailDto)
+        {
+            var planDetail = _mapper.Map<PlanDetail>(planDetailDto);
+            await _unitOfWork.PlanDetails.AddAsync(planDetail);
+            await _unitOfWork.CompleteAsync();
+            return CreatedAtAction(nameof(GetType), new { id = planDetail.DetailId }, planDetail);
+        }
+
+        [HttpDelete]
+        [Route("/planDetail/delete{id}")]
+        public async Task<ActionResult> DeletePlanDetail(int id)
+        {
+            var planDetail = await _unitOfWork.PlanDetails.GetByIdAsync(id);
+            if (planDetail == null)
+            {
+                return NotFound("nothing found on this id");
+            }
+
+            //külön szál vagy se?
+            var result = _unitOfWork.PlanDetails.Remove(planDetail);
+
+            await _unitOfWork.CompleteAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/planDetail/update{id}")]
+        public async Task<ActionResult<PlanDetailDto>> UpdatePlanDetail([FromBody] PlanDetailDto planDetailDto)
+        {
+            var planDetail = _mapper.Map<PlanDetail>(planDetailDto);
+            var updatePlanDetail = _unitOfWork.PlanDetails.Update(planDetail);
+            await _unitOfWork.CompleteAsync();
+            var updatedDto = _mapper.Map<PlanDetailDto>(updatePlanDetail.Entity);
+            return Ok(updatedDto);
+        }
+    }
+}
---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Backend.Model;
+using Backend.Model.DTO;
+using Backend.Repository;
+using Microsoft.AspNetCore.Mvc;
+using User = Backend.Model.User;
+
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<TypeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UserController(ILogger<TypeController> logger, IConfiguration configuration, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        [Route("/user/{id}")]
+        public async Task<ActionResult<User>> GetType(int id)
+        {
+            var t = await _unitOfWork.Users.GetByIdAsync(id);
+            if (t == null)
+            {
+                return NotFound("no plan with id");
+            }
+
+            return Ok(t);
+        }
+
+        [HttpGet]
+        [Route("/user/all")]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllType()
+        {
+            var users = await _unitOfWork.Users.GetAllAsync();
+            var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+            return Ok(userDtos);
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public async Task<ActionResult<User>> AddUser([FromBody] CUUserDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+            await _unitOfWork.Users.AddAsync(user);
+            await _unitOfWork.CompleteAsync();
+            return CreatedAtAction(nameof(GetType), new { id = user.UserId }, user);
+        }
+
+        [HttpDelete]
+        [Route("/user/delete{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound("nothing found on this id");
+            }
+
+            var result = _unitOfWork.Users.Remove(user);
+
+            await _unitOfWork.CompleteAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("/user/update{id}")]
+        public async Task<ActionResult<UserDto>> UpdateUser([FromBody] UserDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+            var updateUser = _unitOfWork.Users.Update(user);
+            await _unitOfWork.CompleteAsync();
+            var updatedDto = _mapper.Map<UserDto>(updateUser.Entity);
+            return Ok(updatedDto);
+        }
+
+
+
+    }
+
+}
